700f27ba1c8fe168bb5620d4344cd433fdb7378c
pressfforyasuo <ka47@mail.ru>
2022-08-09 19:30:03 +0300

    Initial commit

---
+++ myProject/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  myProject
+//
+//  Created by work on 09.08.2022.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
---
+++ myProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  myProject
+//
+//  Created by work on 09.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    let myView = UIView()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    
+    
+}
